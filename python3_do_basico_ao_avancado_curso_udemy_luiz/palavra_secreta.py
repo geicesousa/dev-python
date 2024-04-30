d55b7cfe023adf37e109ecd55c6d7ba51627f0e6
geicesousa <geicesousa@outlook.com>
2024-04-29 23:26:40 -0300

    desafio palavra secreta

--- python3_do_basico_ao_avancado_curso_udemy_luiz/palavra_secreta.py
+++ python3_do_basico_ao_avancado_curso_udemy_luiz/palavra_secreta.py
@@ -3,3 +3,40 @@
 # a letra está na palavra? exiba a letra
 # a letra não está, exibe * 
 # só pode digitar um letra
+
+print('Bem-vindo ao jogo! Será que você consegue acertar a palavra?!?')
+
+palavra_secreta = 'performace'
+letras_acertadas = ''
+contador = 0
+
+while True:
+
+    letra_digitada = input('Digite uma letra: ').lower()
+
+    if letra_digitada == '':
+        print('Você precisa digitar uma letra') 
+        continue
+
+    if len(letra_digitada) > 1:
+        print('Você deve digitar apenas uma letra') 
+        continue
+
+    if letra_digitada in palavra_secreta:
+        letras_acertadas += letra_digitada
+        contador +=1
+
+    palavra_sendo_formada = ''
+    for letra in palavra_secreta:    
+        if letra in letras_acertadas:
+            palavra_sendo_formada += letra
+        else:
+            palavra_sendo_formada += '*'
+    
+    print(palavra_sendo_formada)
+
+    if palavra_sendo_formada == palavra_secreta:
+        break
+
+print(f'Você acertou a palavra {palavra_secreta.upper()} em {contador} tentativas.')
+        
