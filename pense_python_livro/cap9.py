98545dfde66838fbba7aeaf5f79480d69501bb97
geicesousa <geicesousa@outlook.com>
2024-05-17 14:51:58 -0300

    update pense em python

--- pense_python_livro/cap9.py
+++ pense_python_livro/cap9.py
@@ -1,8 +1,10 @@
 print(open('words.txt').readline())
-print(open('words.txt').readline())
+print(open('words.txt').readline().strip())
 linha = open('words.txt')
 print(linha.readline()) # usando assim, cada vez que chama aparece um linha do arquivo
-print(linha.readline())
+print(linha.readline().strip())
+print()
+
 
 # Escreva um programa que leia words.txt e imprima apenas as palavras com mais de 20 caracteres
 
@@ -20,6 +22,8 @@ def has_no_e(word):
 print(f'''has_no_e
 {has_no_e('geice')}
 {has_no_e('amanda')}''')
+
+print()
 
 # resolução do livro - tem o mesmo resultado
 def has_no_e2(word): 
@@ -70,3 +74,14 @@ print('pato', uses_only('pato', ['l', 'o']))
 
 # Escreva uma função chamada is_abecedarian que retorne True se as letras numa palavra aparecerem em ordem alfabética
 
+def is_abecedarian(word):
+    if sorted(word) == list(word) :
+        print('nesta palavra as letras estão em ordem alfabética')
+        return True
+        
+    else:
+        print('nesta palavra as letras são aleatórias')
+        return False
+print()
+is_abecedarian('gol')
+is_abecedarian('ano')
