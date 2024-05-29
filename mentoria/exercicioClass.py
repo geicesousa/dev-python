caaf560a7b34d849e98cbae10f84eca750d3dc11
geicesousa <geicesousa@outlook.com>
2024-05-28 22:33:21 -0300

    atualização class matilha herança -> composição

--- mentoria/exercicioClass.py
+++ mentoria/exercicioClass.py
@@ -6,7 +6,7 @@ class Cachorro:
         self.idade = randint(1, 25)
         
     def __str__(self):
-        return f'Nome: {self.nome} | Idade: {self.idade}'
+        return f'Nome: {self.nome} | Idade: {randint(1, 25)}'
 
     def get_nome(self):
         return self.nome
@@ -22,28 +22,44 @@ class Cachorro:
         self.idade = idade
         return self.idade
     
-class Matilha(Cachorro):
+class Matilha():
     def __init__(self, nome, tamanho):
-        super().__init__(nome) # usar composição ao invés de herança
+        self.nome = Cachorro(nome)
         self.tamanho = tamanho
         self.matilha = []
-    
     
     def cria_matilha(self):
         contador = 0
         while contador < self.tamanho:
             contador += 1
             # criei um dicionário pq garante pegar nome e idade separados
-            # cachorro = { f'"nome": {self.nome} {contador} , "idade": {randint(1, 25)}'} assim cria um set, um set é uma chave e string
-            cachorro = {"nome": f'{self.nome} {contador}'  , "idade": randint(1, 25)}
+            # cachorro = { f'"nome": {self.nome} {contador} , "idade": {randint(1, 25)}'} assim cria um set, um set é criado assim: {"a", "b", "a"} log={"a", "b"}; mantem apenas valores únicos
+            cachorro = {"nome": f'{self.nome.get_nome()} {contador}'  , "idade": randint(1, 25)}
             self.matilha.append(cachorro)
 
-            
-
         return self.matilha
+    
+    def get_matilha(self):
+        return self.matilha
+    
+    def set_matilha(self, attr, index, value):
+        self.matilha[index][attr] = value
 
+    # armazenar no banco de dados, vulgo txt, para que o dado seja armazenado/persistido
 
-instanciacao = Matilha('racas', 8)
-instanciacao.cria_matilha()
-instanciacao.get_nome()
-print(instanciacao.cria_matilha())
+print('instanciação cachorro')
+cao = Cachorro('matias')
+print(cao)
+print(cao.get_nome())
+print(cao.get_idade())
+print('matilha criada')
+primeira_matilha = Matilha('BúDog', 8)
+print(primeira_matilha.cria_matilha())
+print(primeira_matilha.matilha[2])
+print(primeira_matilha.matilha[2].get('idade'))
+print(primeira_matilha.matilha[2].get('nome'))
+print(primeira_matilha.get_matilha())
+primeira_matilha.set_matilha('nome', 0, 'doguinho')
+primeira_matilha.set_matilha('idade', 0, '19')
+primeira_matilha.set_matilha('idade', 1, 32)
+print(primeira_matilha.get_matilha())
